1afeb262cc6dea09b410ccdc4b9b3e1fb3c6e5bd
kolorszczak <rachuba.michal@gmail.com>
2018-06-16 14:31:12 +0200

    Fixed separator drawable + Deleted unused resource provider

--- app/src/main/java/eu/mihau/randomlist/utils/provider/resource/ResourceProvider.java
+++
@@ -1,8 +0,0 @@
-package eu.mihau.randomlist.utils.provider.resource;
-
-import android.support.annotation.ColorRes;
-
-public interface ResourceProvider {
-
-    int getColor(@ColorRes int resId);
-}
